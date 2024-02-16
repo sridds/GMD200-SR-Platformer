b0132e51e94b7527fe75eaacfc9c0dc794bba189
sridds <seth.riddensdale@mymail.champlain.edu>
2024-02-16 12:53:03 -0500

    New State Machine
    I don't think I'll have time to learn how to use the state machine. For now, it was a nice exercise.

---
+++ SRiddensdale_Platformer/Assets/Scripts/State Machine V2/IState.cs
@@ -0,0 +1,7 @@
+public interface IState
+{
+    void OnEnter();
+    void Update();
+    void FixedUpdate();
+    void OnExit();
+}
---
+++ SRiddensdale_Platformer/Assets/Scripts/State Machine V2/ITransition.cs
@@ -0,0 +1,5 @@
+public interface ITransition
+{
+    IState To { get; }
+    IPredicate Condition { get; }
+}
