ad13f3d6408833c4bb100d80c81760ff746dcfdb
sridds <seth.riddensdale@mymail.champlain.edu>
2024-02-16 20:04:29 -0500

    Added a lose condition
    Currently, the lose condition doesn't do anything yet, but it's setup

--- SRiddensdale_Platformer/Assets/Scripts/Player.cs
+++ SRiddensdale_Platformer/Assets/Scripts/Player.cs
@@ -4,14 +4,57 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField]
+    private float hanklingBubbleTimer = 10.0f;
+
     private Hankling myHankling;
 
     // accessors
     public Hankling MyHankling { get { if (myHankling == null) myHankling = FindObjectOfType<Hankling>(); return myHankling; } }
+
+    Timer activeHanklingTimer;
+
+    private void Start()
+    {
+        MyHankling.OnBubbleEnter += BubbleEntered;
+        MyHankling.OnBubblePopped += BubbleExit;
+    }
+
+    private void Update()
+    {
+        // ensure the timer updates
+        if (activeHanklingTimer != null) activeHanklingTimer.Tick(Time.deltaTime);
+    }
+
+    private void BubbleEntered()
+    {
+        // create a timer for when the hankling enters the bubble
+        activeHanklingTimer = new Timer(hanklingBubbleTimer);
+        activeHanklingTimer.OnTimerEnd += Lose;
+    }
+
+    private void BubbleExit()
+    {
+        if (activeHanklingTimer == null) return;
+
+        // unsubscribe and set null
+        activeHanklingTimer.OnTimerEnd -= Lose;
+        activeHanklingTimer = null;
+    }
+
+    /// <summary>
+    /// Called when the hankling timer expires
+    /// </summary>
+    private void Lose()
+    {
+        Debug.Log("Hankling gone");
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         // restart level if hit hazard
         if (collision.gameObject.tag == "Hazard") GameManager.Instance.RestartLevel();
     }
+
+    
 }
